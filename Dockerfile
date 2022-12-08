e744d84edc272966b3796ef319ed617c54457165
Matias0019 <matiromero0019@gmail.com>
2022-12-08 14:12:37 -0300

    tercer commit

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM amazoncorretto:8-alpine-jdk
+MAINTAINER emaaristimuno
+COPY target/back-0.0.1-SNAPSHOT.jar back-0.0.1-SNAPSHOT.jar
+ENTRYPOINT ["java","-jar","/back-0.0.1-SNAPSHOT.jar"]
--- src/test/java/com/portfolio/back/MainApplicationTests.java
+++ src/test/java/com/portfolio/back/MainApplicationTests.java
@@ -1,10 +1,10 @@
-package com.portfolio.mgb;
+package com.portfolio.back;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MgbApplicationTests {
+class MainApplicationTests {
 
 	@Test
 	void contextLoads() {
