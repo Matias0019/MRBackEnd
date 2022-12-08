84ebfb647cb4a9c32d8ea260447b302fa82e80c6
Matias0019 <matiromero0019@gmail.com>
2022-12-08 16:14:21 -0300

    cuarto commit

--- src/main/java/com/portfolio/back/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/back/Security/Controller/AuthController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.portfolio.back.Security.Controller;
 
 import com.portfolio.back.Security.Dto.JwtDto;
@@ -35,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"direcciondeployfinal","http://localhost:4200"})
+@CrossOrigin(origins = {"https://matias-romero-frontend.web.app","http://localhost:4200"})
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
